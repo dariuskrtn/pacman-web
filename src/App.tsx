736ec6cabd6edb11edfd3700882a1b62a0c74dd0
vstrimaitis <vstrimaitis@gmail.com>
2019-06-07 10:55:45 +0300

    Removed navigation links

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import React, { } from "react";
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import { SubmissionsView } from "./components/views/SubmissionsView";
-import { HashRouter as Router, Route } from "react-router-dom";
-import { NewProgramView } from "./components/views/NewProgramView";
-import { Header } from "./components/Header";
-import { ScoreboardView } from "./components/views/ScoreboardView";
 
 const App = () => {
     return (
         <Router>
             <div>
-                <Header />
-                <Route exact path="/" component={SubmissionsView} />
-                <Route path="/submit" component={NewProgramView} />
-                <Route path="/scoreboard" component={ScoreboardView} />
+                {/* <Header /> */}
+                <Route exact={true} path="/" component={SubmissionsView} />
+                {/* <Route path="/submit" component={NewProgramView} /> */}
+                {/* <Route path="/scoreboard" component={ScoreboardView} /> */}
             </div>
         </Router>
-    )
+    );
 };
 
 export default App;
