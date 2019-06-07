0404a26350c7dfa204008f3d8ca24324ad748385
vstrimaitis <vstrimaitis@gmail.com>
2019-06-07 16:50:50 +0300

    Increased queue font size, added a bettter empty queue indicator

--- src/components/queue/SubmissionQueue.tsx
+++ src/components/queue/SubmissionQueue.tsx
@@ -1,3 +1,5 @@
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Outcome, Submission, SubmissionDetailsResponse } from "../../api/Contracts";
 
@@ -18,28 +20,57 @@ interface SubmissionQueueProps {
     items: QueueItem[];
 }
 
+const outcomeToString = (outcome: Outcome) => {
+    switch (outcome) {
+        case Outcome.success:
+            return "OK";
+        case Outcome.fail:
+            return "Failed";
+        case Outcome.outOfMoves:
+            return "Out of moves";
+    }
+};
+
 // TODO: uzdet atitinkamas klases, kad paspalvintu or sth
 const renderQueueItem = (item: QueueItem) => {
     const text = `${item.submission.id}. ${item.submission.user}`;
     const classes = ["list-group-item"];
+    let status = "Judging";
     if (item.details && item.state === QueueItemState.SIMULATING) {
         classes.push("list-group-item-primary");
     }
     if (item.details && item.state === QueueItemState.SHOW_OUTCOME) {
+        status = outcomeToString(item.details.outcome);
         if (item.details.outcome === Outcome.success) {
             classes.push("list-group-item-success");
-        } else if(item.details.outcome === Outcome.fail) {
+        } else if (item.details.outcome === Outcome.fail) {
             classes.push("list-group-item-danger");
         } else {
             classes.push("list-group-item-warning");
         }
     }
-    return <li className={classes.join(" ")} key={item.submission.id}>{text}</li>;
-}
+    return (
+        <li className={classes.join(" ")} key={item.submission.id}>
+            <h3 className="display-4">{text}</h3>
+            <h3><small className="text-muted">{status}</small></h3>
+        </li>
+    );
+};
+
+const emptyQueueMessage = (
+    <ul className="list-group">
+        <li className="list-group-item list-group-item-light">
+            <h3 className="display-5">
+                <FontAwesomeIcon icon={faClock} style={{marginRight: "10px"}} />
+                Waiting for submissions
+            </h3>
+        </li>
+    </ul>
+);
 
 export const SubmissionQueue = ({ items }: SubmissionQueueProps) => (
     items.length === 0
-        ? <p>Queue is empty :)</p>
+        ? emptyQueueMessage
         : (
             <ul className="list-group">
                 {items.map(renderQueueItem)}
